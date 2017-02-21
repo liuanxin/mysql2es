b3c07d797ef813e4e40157e219bdac6dbf5dc81a
Tony Liu <liu_anxin@163.com>
2017-02-21 18:03:08 +0800

    fix rebuild

--- src/main/java/com/github/mte/db/DataInit.java
+++ src/main/java/com/github/mte/db/DataInit.java
@@ -2,12 +2,19 @@ package com.github.mte.db;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.github.mte.model.Config;
+import com.github.mte.util.U;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.InetSocketAddress;
 
 @Configuration
 public class DataInit {
@@ -18,9 +25,31 @@ public class DataInit {
         return new Config();
     }
 
-    @Bean(value = "dataSource")
+    @Bean
     @ConfigurationProperties(prefix = "db")
     public DataSource setup() {
         return DataSourceBuilder.create().type(DruidDataSource.class).build();
+    }
+
+    @Bean(destroyMethod = "close")
+    public TransportClient connect() {
+        Config config = config();
+        U.assertNil(config, "no config with mysql and es mapping");
+        config.check();
+
+        // es 5.1.1 --> TransportClient is a abstract class
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
+        for (String ipAndPort : config.getIpPort()) {
+            String[] ipPort = ipAndPort.split(":");
+            if (U.isNotBlank(ipPort) && ipPort.length >= 2) {
+                String ip = ipPort[0];
+                int port = NumberUtils.toInt(ipPort[1]);
+
+                if (U.isNotBlank(ip) && U.greater0(port)) {
+                    client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(ip, port)));
+                }
+            }
+        }
+        return client;
     }
 }
--- src/main/java/com/github/mte/repository/EsRepository.java
+++ src/main/java/com/github/mte/repository/EsRepository.java
@@ -3,25 +3,19 @@ package com.github.mte.repository;
 import com.github.mte.model.Config;
 import com.github.mte.model.Document;
 import com.github.mte.model.Scheme;
+import com.github.mte.util.A;
 import com.github.mte.util.Jsons;
 import com.github.mte.util.Logs;
-import com.github.mte.util.U;
-import com.github.mte.util.A;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 
 @Component
@@ -30,46 +24,27 @@ public class EsRepository {
     @Autowired
     private Config config;
 
-    private TransportClient connect() {
-        U.assertNil(config, "no config with mysql and es mapping");
-        config.check();
-
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
-        for (String ipAndPort : config.getIpPort()) {
-            String[] ipPort = ipAndPort.split(":");
-            if (U.isNotBlank(ipPort) && ipPort.length >= 2) {
-                String ip = ipPort[0];
-                int port = NumberUtils.toInt(ipPort[1]);
-
-                if (U.isNotBlank(ip) && U.greater0(port)) {
-                    client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(ip, port)));
-                }
-            }
-        }
-        return client;
-    }
+    @Autowired
+    private TransportClient client;
 
     @Async
     public void deleteScheme(List<Scheme> schemes) {
         if (A.isNotEmpty(schemes)) {
-            // try-with-resources
-            try (TransportClient client = connect()) {
-                IndicesAdminClient indices = client.admin().indices();
+            IndicesAdminClient indices = client.admin().indices();
 
-                for (Scheme scheme : schemes) {
-                    String index = scheme.getIndex();
-                    String type = scheme.getType();
-                    try {
-                        if (indices.prepareExists(index).get().isExists()) {
-                            DeleteIndexResponse resp = indices.prepareDelete(index).get();
-                            boolean flag = resp.isAcknowledged();
-                            if (Logs.ROOT_LOG.isDebugEnabled())
-                                Logs.ROOT_LOG.debug("delete scheme ({}/{}) return: ({})", index, type, flag);
-                        }
-                    } catch (ElasticsearchException e) {
-                        if (Logs.ROOT_LOG.isWarnEnabled())
-                            Logs.ROOT_LOG.warn(String.format("delete scheme (%s/%s) es exception", index, type), e);
+            for (Scheme scheme : schemes) {
+                String index = scheme.getIndex();
+                String type = scheme.getType();
+                try {
+                    if (indices.prepareExists(index).get().isExists()) {
+                        DeleteIndexResponse resp = indices.prepareDelete(index).get();
+                        boolean flag = resp.isAcknowledged();
+                        if (Logs.ROOT_LOG.isDebugEnabled())
+                            Logs.ROOT_LOG.debug("delete scheme ({}/{}) return: ({})", index, type, flag);
                     }
+                } catch (ElasticsearchException e) {
+                    if (Logs.ROOT_LOG.isWarnEnabled())
+                        Logs.ROOT_LOG.warn(String.format("delete scheme (%s/%s) es exception", index, type), e);
                 }
             }
         }
@@ -80,37 +55,33 @@ public class EsRepository {
         if (A.isNotEmpty(schemes)) {
             List<Scheme> successList = A.lists();
 
-            // try-with-resources
-            try (TransportClient client = connect()) {
-                IndicesAdminClient indices = client.admin().indices();
-
-                for (Scheme scheme : schemes) {
-                    String index = scheme.getIndex();
-                    String type = scheme.getType();
-                    try {
-                        // create index if not exists
-                        if (!indices.prepareExists(index).get().isExists()) {
-                            indices.prepareCreate(index).get();
-                        }
-                        // indices.prepareDelete(index).get().isAcknowledged()
-
-                        String source = Jsons.toJson(A.maps("properties", scheme.getProperties()));
-                        if (Logs.ROOT_LOG.isDebugEnabled()) {
-                            Logs.ROOT_LOG.debug("curl -XPUT \"http://{}/{}/{}/_mapping\" -d '{}'",
-                                    config.ipAndPort(), index, type, source);
-                        }
-                        // create or update mapping
-                        PutMappingResponse resp = indices.preparePutMapping(index).setType(type).setSource(source).get();
-                        boolean flag = resp.isAcknowledged();
-                        if (flag) {
-                            successList.add(scheme);
-                        }
-                        if (Logs.ROOT_LOG.isDebugEnabled())
-                            Logs.ROOT_LOG.debug("put scheme ({}/{}) return: ({})", index, type, flag);
-                    } catch (ElasticsearchException e) {
-                        if (Logs.ROOT_LOG.isWarnEnabled())
-                            Logs.ROOT_LOG.warn(String.format("put scheme (%s/%s) es exception", index, type), e);
+            IndicesAdminClient indices = client.admin().indices();
+            for (Scheme scheme : schemes) {
+                String index = scheme.getIndex();
+                String type = scheme.getType();
+                try {
+                    // create index if not exists
+                    if (!indices.prepareExists(index).get().isExists()) {
+                        indices.prepareCreate(index).get();
                     }
+                    // indices.prepareDelete(index).get().isAcknowledged()
+
+                    String source = Jsons.toJson(A.maps("properties", scheme.getProperties()));
+                    if (Logs.ROOT_LOG.isDebugEnabled()) {
+                        Logs.ROOT_LOG.debug("curl -XPUT \"http://{}/{}/{}/_mapping\" -d '{}'",
+                                config.ipAndPort(), index, type, source);
+                    }
+                    // create or update mapping
+                    PutMappingResponse resp = indices.preparePutMapping(index).setType(type).setSource(source).get();
+                    boolean flag = resp.isAcknowledged();
+                    if (flag) {
+                        successList.add(scheme);
+                    }
+                    if (Logs.ROOT_LOG.isDebugEnabled())
+                        Logs.ROOT_LOG.debug("put scheme ({}/{}) return: ({})", index, type, flag);
+                } catch (ElasticsearchException e) {
+                    if (Logs.ROOT_LOG.isWarnEnabled())
+                        Logs.ROOT_LOG.warn(String.format("put scheme (%s/%s) es exception", index, type), e);
                 }
             }
 
@@ -125,21 +96,20 @@ public class EsRepository {
     public void saveDataToEs(List<Document> documents) {
         if (A.isNotEmpty(documents)) {
             List<Document> successList = A.lists();
-            try (TransportClient client = connect()) {
-                for (Document doc : documents) {
-                    try {
-                        IndexResponse response = client.prepareIndex(doc.getIndex(), doc.getType(), doc.getId())
-                                .setSource(Jsons.toJson(doc.getData())).get();
-                        if (Logs.ROOT_LOG.isDebugEnabled())
-                            Logs.ROOT_LOG.debug("create or update date return : {}", response.status());
+            for (Document doc : documents) {
+                try {
+                    IndexResponse response = client.prepareIndex(doc.getIndex(), doc.getType(), doc.getId())
+                            .setSource(Jsons.toJson(doc.getData())).get();
+                    if (Logs.ROOT_LOG.isDebugEnabled())
+                        Logs.ROOT_LOG.debug("create or update date return : {}", response.status());
 
-                        successList.add(doc);
-                    } catch (ElasticsearchException e) {
-                        if (Logs.ROOT_LOG.isWarnEnabled())
-                            Logs.ROOT_LOG.warn("create or update data es exception", e);
-                    }
+                    successList.add(doc);
+                } catch (ElasticsearchException e) {
+                    if (Logs.ROOT_LOG.isWarnEnabled())
+                        Logs.ROOT_LOG.warn("create or update data es exception", e);
                 }
             }
+
             if (A.isNotEmpty(successList)) {
                 if (Logs.ROOT_LOG.isInfoEnabled())
                     Logs.ROOT_LOG.info("put {} {} documents from db to es", successList.size(), successList);
