d8aa8361fad4b4de130b2067b7badad0b0e464c4
liujie <liu_anxin@163.com>
2018-11-19 13:05:25 +0800

    fix

--- src/main/java/com/github/util/Dates.java
+++ src/main/java/com/github/util/Dates.java
@@ -1,6 +1,5 @@
 package com.github.util;
 
-import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -13,6 +12,15 @@ import java.util.Locale;
 public class Dates {
 
     public enum Type {
+        /** yyyy-MM-ddTHH:mm:ss.SSSZ */
+        TSZ("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
+        /** yyyy-MM-ddTHH:mm:ss.SSS */
+        TS("yyyy-MM-dd'T'HH:mm:ss.SSS"),
+        /** yyyy-MM-ddTHH:mm:ssZ */
+        TZ("yyyy-MM-dd'T'HH:mm:ss'Z'"),
+        /** yyyy-MM-ddTHH:mm:ss */
+        T("yyyy-MM-dd'T'HH:mm:ss"),
+
         /** yyyy-MM-dd HH:mm:ss SSS */
         YYYY_MM_DD_HH_MM_SS_SSS("yyyy-MM-dd HH:mm:ss SSS"),
         /** yyyy-MM-dd HH:mm:ss */
@@ -23,8 +31,6 @@ public class Dates {
         YYYY_MM_DD("yyyy-MM-dd"),
         /** yyyy-MM */
         YYYY_MM("yyyy-MM"),
-        /** yyyy-MM-dd am/pm --> am/pm 会根据时区自动完成, 也就是如果当前时区是北京的话, 会显示成 上午/下午 */
-        YYYY_MM_DD_AP("yyyy-MM-dd a"),
 
         /** yyyyMMddHHmmssSSS */
         YYYYMMDDHHMMSSSSS("yyyyMMddHHmmssSSS"),
@@ -44,29 +50,11 @@ public class Dates {
         /** HH:mm */
         HH_MM("HH:mm"),
 
-        /** 到毫秒: yyyy-MM-ddTHH:mm:ss.SSSZ */
-        TSZ("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
-        /** 到毫秒: yyyy-MM-ddTHH:mm:ss.SSS */
-        TS("yyyy-MM-dd'T'HH:mm:ss.SSS"),
-        /** 到秒: yyyy-MM-ddTHH:mm:ssZ */
-        TZ("yyyy-MM-dd'T'HH:mm:ss'Z'"),
-        /** 到秒: yyyy-MM-ddTHH:mm:ss */
-        T("yyyy-MM-dd'T'HH:mm:ss"),
-
         /** yyyy/MM/dd */
         USA_YYYY_MM_DD("yyyy/MM/dd"),
         /** MM/dd/yyyy HH:mm:ss */
         USA_MM_DD_YYYY_HH_MM_SS("MM/dd/yyyy HH:mm:ss"),
-        /** yyyy年MM月dd日 HH时mm分ss秒 */
-        CN_YYYY_MM_DD_HH_MM_SS("yyyy年MM月dd日 HH时mm分ss秒"),
-        /** yyyy年MM月dd日 HH点 */
-        CN_YYYY_MM_DD_HH("yyyy年MM月dd日 HH点"),
-        /** yyyy年MM月dd日 HH点 */
-        CN_YYYY_MM_DD_HH_MM("yyyy年MM月dd日 HH点mm分"),
-        /** yyyy年MM月dd日 */
-        CN_YYYY_MM_DD("yyyy年MM月dd日"),
 
-        /** 直接打印 new Date() 时的样式 */
         CST("EEE MMM dd HH:mm:ss zzz yyyy");
 
         private String value;
@@ -90,9 +78,17 @@ public class Dates {
     public static String format(Date date, Type type) {
         if (U.isBlank(date) || U.isBlank(type)) {
             return U.EMPTY;
+        } else {
+            return getDateFormat(type).print(date.getTime());
         }
+    }
 
-        return getDateFormat(type).print(date.getTime());
+    public static String format(Date date, String type) {
+        if (U.isBlank(date) || U.isBlank(type)) {
+            return U.EMPTY;
+        } else {
+            return DateTimeFormat.forPattern(type).print(date.getTime());
+        }
     }
 
     private static DateTimeFormatter getDateFormat(Type type) {
@@ -122,26 +118,5 @@ public class Dates {
             }
         }
         return null;
-    }
-
-    /** 2016-12-31 xx:yy:zz --> 2016-12-31 00:00:00 000 */
-    public static Date startInDay(Date date) {
-        if (U.isBlank(date)) {
-            return null;
-        }
-        return new DateTime(date).hourOfDay().withMinimumValue()
-                .minuteOfHour().withMinimumValue()
-                .secondOfMinute().withMinimumValue()
-                .millisOfSecond().withMinimumValue().toDate();
-    }
-    /** 2016-12-31 xx:yy:zz --> 2016-12-31 23:59:59 999 */
-    public static Date endInDay(Date date) {
-        if (U.isBlank(date)) {
-            return null;
-        }
-        return new DateTime(date).hourOfDay().withMaximumValue()
-                .minuteOfHour().withMaximumValue()
-                .secondOfMinute().withMaximumValue()
-                .millisOfSecond().withMaximumValue().toDate();
     }
 }
