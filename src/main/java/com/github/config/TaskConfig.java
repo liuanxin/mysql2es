2b5659f3188f688ed18691300780d9cf7faa4767
liujie <liu_anxin@163.com>
2018-12-19 16:16:26 +0800

    fix

--- src/main/java/com/github/config/TaskConfig.java
+++ src/main/java/com/github/config/TaskConfig.java
@@ -1,7 +1,11 @@
 package com.github.config;
 
+import com.github.model.Config;
+import com.github.util.A;
+import com.github.util.U;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,13 +19,23 @@ public class TaskConfig implements AsyncConfigurer {
 
     static final int PROCESSORS = Runtime.getRuntime().availableProcessors();
 
+    @Autowired
+    private Config config;
+
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(PROCESSORS);
-        executor.setMaxPoolSize(PROCESSORS << 2);
-        executor.setQueueCapacity(PROCESSORS);
-        executor.setThreadNamePrefix("task-executor-");
+        if (U.isBlank(config) || A.isEmpty(config.getRelation())) {
+            executor.setCorePoolSize(PROCESSORS);
+            executor.setMaxPoolSize(PROCESSORS << 2);
+            executor.setQueueCapacity(PROCESSORS);
+        } else {
+            int size = config.getRelation().size();
+            executor.setCorePoolSize(size);
+            executor.setMaxPoolSize(size << 2);
+            executor.setQueueCapacity(size);
+        }
+        executor.setThreadNamePrefix("async-executor-");
         executor.initialize();
         return executor;
     }
