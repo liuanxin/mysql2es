721ea6f61c8e05a9db07c4d22d3b27a92139afff
TY <liu_anxin@163.com>
2021-01-11 19:44:50 +0800

    fix

--- src/main/java/com/github/run/Runner.java
+++ src/main/java/com/github/run/Runner.java
@@ -1,17 +1,19 @@
 package com.github.run;
 
-import com.github.model.Config;
 import com.github.model.Relation;
 import com.github.repository.DataRepository;
 import com.github.repository.EsRepository;
 import com.github.util.A;
 import com.github.util.Dates;
 import com.github.util.Logs;
+import com.github.util.U;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.List;
 import java.util.Map;
 
 @Profile("!test")
@@ -19,11 +21,12 @@ import java.util.Map;
 @SuppressWarnings("rawtypes")
 public class Runner implements ApplicationRunner {
 
-    private final Config config;
+    @Value("${db2es.relation}")
+    private List<Relation> relations;
+
     private final EsRepository esRepository;
     private final DataRepository dataRepository;
-    public Runner(Config config, EsRepository esRepository, DataRepository dataRepository) {
-        this.config = config;
+    public Runner(EsRepository esRepository, DataRepository dataRepository) {
         this.esRepository = esRepository;
         this.dataRepository = dataRepository;
     }
@@ -31,12 +34,17 @@ public class Runner implements ApplicationRunner {
     @SuppressWarnings("RedundantThrows")
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        U.assertException(A.isEmpty(relations), "must set [db es] relation");
+        for (Relation r : relations) {
+            r.check();
+        }
+
         long start = System.currentTimeMillis();
         if (Logs.ROOT_LOG.isInfoEnabled()) {
             Logs.ROOT_LOG.info("begin to generate scheme");
         }
         try {
-            for (Relation relation : config.getRelation()) {
+            for (Relation relation : relations) {
                 Map<String, Map> properties = dataRepository.dbToEsScheme(relation);
                 if (relation.isScheme() && A.isNotEmpty(properties)) {
                     String index = relation.useIndex();
