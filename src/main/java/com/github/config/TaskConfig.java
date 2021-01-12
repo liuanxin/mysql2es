972789b7d5fb82fa72ee3fe33aaad2db0631bcd6
liuanxin <liu_anxin@163.com>
2021-01-12 18:09:29 +0800

    fix

--- src/main/java/com/github/config/TaskConfig.java
+++ src/main/java/com/github/config/TaskConfig.java
@@ -1,6 +1,9 @@
 package com.github.config;
 
+import com.github.model.Config;
+import com.github.util.A;
 import com.github.util.U;
+import lombok.AllArgsConstructor;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
@@ -12,17 +15,28 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
+@AllArgsConstructor
 public class TaskConfig implements AsyncConfigurer {
+
+    private final Config config;
 
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
-        int size = U.PROCESSORS;
-        executor.setCorePoolSize(size);
-        executor.setMaxPoolSize(size << 2);
-        executor.setQueueCapacity(size);
-        executor.setThreadNamePrefix("async-executor-");
+        if (U.isNotBlank(config) && A.isNotEmpty(config.getRelation())) {
+            int size = config.getRelation().size();
+            executor.setCorePoolSize(size);
+            executor.setMaxPoolSize(size);
+            executor.setQueueCapacity(0);
+        } else {
+            int size = U.PROCESSORS;
+            executor.setCorePoolSize(size);
+            executor.setMaxPoolSize(size << 2);
+            executor.setQueueCapacity(size);
+        }
+
+        executor.setThreadNamePrefix("mysql2es-executor-");
         executor.initialize();
 
         return executor;
