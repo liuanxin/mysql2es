c4cba5ca0b8c05b22bc4504e208f60f850067a85
TY <liu_anxin@163.com>
2021-02-26 14:52:55 +0800

    fix

--- src/main/java/com/github/repository/DataRepository.java
+++ src/main/java/com/github/repository/DataRepository.java
@@ -37,6 +37,8 @@ public class DataRepository {
             "CREATE TABLE IF NOT EXISTS `t_db_to_es` (" +
             "  `table_index` VARCHAR(64) NOT NULL," +
             "  `increment_value` VARCHAR(256) NOT NULL," +
+            "  `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+            "  `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
             "  PRIMARY KEY (`table_index`)" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
     private static final String ADD_INCREMENT = "REPLACE INTO `t_db_to_es`(`table_index`, `increment_value`) VALUES(?, ?)";
@@ -292,7 +294,7 @@ public class DataRepository {
                 return;
             } else {
                 // write current equals record
-                String valueToSave = tempColumnValue + EQUALS_SPLIT + i + EQUALS_SUFFIX;
+                String valueToSave = equalsValue + EQUALS_SPLIT + i + EQUALS_SUFFIX;
                 saveLastValue(incrementType, matchTable, relation.getIncrementColumn(), index, valueToSave);
             }
         }
