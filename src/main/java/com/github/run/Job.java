73e82fbf1e17766e76cdb5dba8095ea67a7f2a9d
TY <liu_anxin@163.com>
2021-05-07 18:01:33 +0800

    fix

--- src/main/java/com/github/run/Job.java
+++ src/main/java/com/github/run/Job.java
@@ -3,7 +3,8 @@ package com.github.run;
 import com.github.model.Config;
 import com.github.service.SyncDataService;
 import com.github.util.U;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -17,9 +18,12 @@ import org.springframework.scheduling.support.CronTrigger;
  * @see org.springframework.boot.autoconfigure.task.TaskSchedulingProperties
  */
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 @SuppressWarnings("NullableProblems")
 public class Job implements SchedulingConfigurer {
+
+    @Value("${db2es.crontabPoolSize:0}")
+    private int crontabPoolSize;
 
     private final Config config;
     private final SyncDataService syncService;
@@ -28,15 +32,15 @@ public class Job implements SchedulingConfigurer {
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         if (config.isEnable()) {
             ThreadPoolTaskScheduler crontab = new ThreadPoolTaskScheduler();
-            crontab.setPoolSize(U.PROCESSORS << 2);
+            crontab.setPoolSize((crontabPoolSize <= 0) ? (U.PROCESSORS << 2) : crontabPoolSize);
             crontab.setThreadNamePrefix("mysql2es-crontab-");
             crontab.initialize();
             taskRegistrar.setTaskScheduler(crontab);
 
-            taskRegistrar.addTriggerTask(syncService::handle, new CronTrigger(config.getCron()));
             if (config.isEnableCompensate()) {
                 taskRegistrar.addTriggerTask(syncService::handleCompensate, new CronTrigger(config.getCompensateCron()));
             }
+            taskRegistrar.addTriggerTask(syncService::handle, new CronTrigger(config.getCron()));
         }
     }
 }
